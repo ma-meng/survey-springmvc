157823cf9060324ddb85a0afa2782a21726c3c39
mameng <18045142590@163.com>
2020-01-29 23:48:13 +0800

    自定义字符编码转换器：解决POST请求后台字符乱码问题

---
+++ src/main/java/com/ma/survey/filter/MyCharacterEncodingFilter.java
@@ -0,0 +1,19 @@
+package com.ma.survey.filter;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * 自定义字符编码转换器：解决POST请求后台字符乱码问题
+ * created by <a href="mailto:18045142590@163.com" > mameng </a>
+ */
+@Component
+@WebFilter("/*")
+public class MyCharacterEncodingFilter extends CharacterEncodingFilter {
+    private static String DEFAULT_ENCODING = "UTF-8";
+    public MyCharacterEncodingFilter() {
+        super(DEFAULT_ENCODING);
+    }
+}
